a3715c45716b4902d6202713b475db8f0982d25c
Bradford Knowlton <brad@ucanbonline.net>
2016-03-04 11:33:42 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# WP-Customization-Plugin
+=== Plugin Name ===
+Contributors: bradmkjr
+Donate link: http://bradknowlton.com
+Tags: tags
+Requires at least: 4.4.2
+Tested up to: 5.0
+Stable tag: 1.0.1
+
+Here is a short description of the plugin. This should be no more than 150 chars. No markup here.
+
+== Description ==
+
+This is the long description. No limit, and you can use Markdown (as well as in the following sections).
+
+For backwards compatibility, if this section is missing, the full length of the short description will be used, and
+Markdown parsed.
---
+++ wp-customization-plugin.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+	Plugin Name: WP Customization Plugin
+	Plugin URI:  http://redbrickstrategies.com/
+	Description: Adds custom JS and CSS files to site, without having to edit theme.
+	Version:     1.0.1
+	Author:      Bradford Knowlton
+	Author URI:  http://bradknowlton.com
+	GitHub Plugin URI: https://github.com/RedBrickStrategies/WP-Customization-Plugin
+*/
+
+define( 'WCP_PLUGIN_VERSION', '1.0.1');
+
+/**
+ * Enqueue scripts
+ *
+ * @param string $handle Script name
+ * @param string $src Script url
+ * @param array $deps (optional) Array of script names on which this script depends
+ * @param string|bool $ver (optional) Script version (used for cache busting), set to null to disable
+ * @param bool $in_footer (optional) Whether to enqueue the script before </head> or before </body>
+ */
+function wcp_frontend_enqueue_scripts() {
+    wp_enqueue_script( 'wcp-scripts', plugins_url( '/js/scripts.js' , __FILE__ ), array(), WCP_PLUGIN_VERSION, false);
+    wp_enqueue_style( 'wcp-styles', plugins_url( '/css/styles.css' , __FILE__ ), array(), WCP_PLUGIN_VERSION );
+    // Localize the script with new data
+    /*
+    $translation_array = array(
+        'New_Friend_Added' => __( 'New Friend Added', 'wcp' ),
+    );
+    wp_localize_script( 'wcp-scripts-localize', 'wcp_strings', $translation_array );
+    wp_enqueue_script( 'wcp-scripts-localize' );
+    */
+}
+
+add_action( 'wp_enqueue_scripts', 'wcp_frontend_enqueue_scripts' );	
